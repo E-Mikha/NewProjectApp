f9cd1c8159c0199e9d08d1aaae189d538910aef5
Mikhail Elanov <Elanov1991@Gmail.com>
2024-07-11 21:09:20 +0700

    Fix

--- js/index.js
+++ js/index.js
@@ -2,10 +2,38 @@ const resultElement = document.getElementById('result');
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const submitBtn = document.getElementById('submit');
+const plusBtn = document.getElementById('plus');
+const minusBtn = document.getElementById('minus');
+let action = '+';
 
-const sum = Number(input1.value) + Number(input2.value);
-resultElement.textContent = sum;
+plusBtn.onclick = function () {
+  action = '+';
+};
 
-submitBtn.addEventListener('click', () => {
-  console.log('click');
-});
+minusBtn.onclick = function () {
+  action = '-';
+};
+
+submitBtn.onclick = function () {
+  if (action == '+') {
+    const sum = Number(input1.value) + Number(input2.value);
+    resultElement.textContent = sum;
+  }
+
+  if (action == '-') {
+    const sum = Number(input1.value) - Number(input2.value);
+    resultElement.textContent = sum;
+  }
+};
+
+// plusBtn.addEventListener('click', () => {
+//   console.log('+');
+// });
+
+// minusBtn.addEventListener('click', () => {
+//   console.log('-');
+// });
+
+// submitBtn.addEventListener('click', () => {
+//   console.log('click');
+// });
