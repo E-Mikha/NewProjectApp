687f4cf7685ecd492ba1292ada4e8c655d408465
Mikhail Elanov <Elanov1991@Gmail.com>
2024-07-11 20:51:09 +0700

    Fix

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-let num = 42;
-let firstName = 'Mikhail';
-const isProgramer = true;
+const resultElement = document.getElementById('result');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const submitBtn = document.getElementById('submit');
+
+const sum = Number(input1.value) + Number(input2.value);
+resultElement.textContent = sum;
+
+submitBtn.addEventListener('click', () => {
+  console.log('click');
+});
