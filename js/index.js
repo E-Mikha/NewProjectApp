e182b5e44bd5883fbe4146cbf7e76a8feaed5f59
Mikhail Elanov <Elanov1991@Gmail.com>
2024-07-12 09:45:26 +0700

    Add new btn

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const input2 = document.getElementById('input2');
 const submitBtn = document.getElementById('submit');
 const plusBtn = document.getElementById('plus');
 const minusBtn = document.getElementById('minus');
+const multiplyBtn = document.getElementById('multiply');
+const divideBtn = document.getElementById('divide');
 let action = '+';
 
 plusBtn.onclick = function () {
@@ -14,26 +16,39 @@ minusBtn.onclick = function () {
   action = '-';
 };
 
-submitBtn.onclick = function () {
-  if (action == '+') {
-    const sum = Number(input1.value) + Number(input2.value);
-    resultElement.textContent = sum;
-  }
-
-  if (action == '-') {
-    const sum = Number(input1.value) - Number(input2.value);
-    resultElement.textContent = sum;
-  }
+multiplyBtn.onclick = function () {
+  action = '*';
 };
 
-// plusBtn.addEventListener('click', () => {
-//   console.log('+');
-// });
+divideBtn.onclick = function () {
+  action = '/';
+};
 
-// minusBtn.addEventListener('click', () => {
-//   console.log('-');
-// });
+function printResult(result) {
+  if (result < 0) {
+    resultElement.style.color = 'red';
+  } else {
+    resultElement.style.color = 'green';
+  }
+  resultElement.textContent = result;
+}
 
-// submitBtn.addEventListener('click', () => {
-//   console.log('click');
-// });
+function computedNumberWithAction(inp1, inp2, actionSymbol) {
+  const num1 = Number(inp1.value);
+  const num2 = Number(inp2.value);
+
+  if (actionSymbol == '+') {
+    return num1 + num2;
+  } else if (actionSymbol == '-') {
+    return num1 - num2;
+  } else if (actionSymbol == '*') {
+    return num1 * num2;
+  } else if (actionSymbol == '/') {
+    return num1 / num2;
+  }
+}
+
+submitBtn.onclick = function () {
+  const result = computedNumberWithAction(input1, input2, action);
+  printResult(result);
+};
